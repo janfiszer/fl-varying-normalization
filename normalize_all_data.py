de52d123f7522acb9416b8d6864e6304e7dd29dd
JanFiszer <jf.janfiszer@gmail.com>
2025-04-02 12:38:51 +0200

    normalizaion for multiple methods working

--- normalize_all_data.py
+++ normalize_all_data.py
@@ -1,9 +1,12 @@
-from typing import Dict
+import logging
+from typing import Dict, List
+from collections.abc import Callable
 
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+import configs.config
 from src.utils import files_operations as fop
 
 import nibabel as nib
@@ -11,20 +14,67 @@ from intensity_normalization.typing import Modality, TissueType
 from intensity_normalization.normalize.fcm import FCMNormalize
 from intensity_normalization.normalize.nyul import NyulNormalize
 from intensity_normalization.normalize.whitestripe import WhiteStripeNormalize
+from intensity_normalization.normalize.zscore import ZScoreNormalize
+from intensity_normalization.normalize.ravel import RavelNormalize
 from intensity_normalization.normalize.kde import KDENormalize
 
 
-class Normalizer:
-    def __init__(self, name, normalizing_function, requirements=None):
-        self.name = name
-        self.function = normalizing_function
-        self.requirements = requirements
+class NoNormalization:
+    def __init__(self):
+        return
 
-    def __call__(self, image):
-        return self.function(image)
+    def __call__(self, image, *arg, **kwargs):
+        return image
+
+
+class Normalizer:
+    def __init__(self,
+                 name: str,
+                 normalizer,
+                 normalizer_kwargs: Dict = None,
+                 before_each_normalization_func: Callable[Dict] = None,
+                 requirements=None):
+
+        if normalizer_kwargs is None:
+            normalizer_kwargs = {}
+
+        self.name = name
+        self.normalizer = normalizer(**normalizer_kwargs)
+        self.requirements = requirements
+        self.before_each_normalization_func = before_each_normalization_func
+
+    def setup(self, *args):
+        if self.name == "nyul":
+            self.nyul_setup(*args)
+        else:
+            logging.log(logging.INFO, f"Nothing to set up in {self.name} normalizer")
+
+    def __call__(self, image,
+                 modality: str,
+                 # before_each_func_kwargs: Dict
+                 ):
+
+        kwargs = {"modality": modality}
+        # so far skipping FCM and no playing with each iteration adaptation
+        # for parameter, value in self.before_each_normalization_func(**before_each_func_kwargs):
+        #     kwargs[parameter] = value
+
+        return self.normalizer(image, **kwargs)
 
     def __str__(self):
         return self.name
+
+    def nyul_setup(self, other_images):
+        # loading the images
+        self.normalizer.fit(other_images)
+
+        return {}
+
+    def fcm_each_normalization(self, t1_image):
+        # TODO: double check if it should be done at each time
+        _ = self.normalizer(t1_image)
+
+        return {}
 
 
 def plot_histogram(image, title=""):
@@ -39,28 +89,31 @@ def normalize_fcm(image_to_normalize, t1_image, modality):
     fcm_norm = FCMNormalize(tissue_type=TissueType.WM)
     _ = fcm_norm(t1_image)
 
-    fcm_flair = fcm_norm(image_to_normalize, modality=modality)
+    normalized_image = fcm_norm(image_to_normalize, modality=modality)
 
-    return fcm_flair
+    return normalized_image
 
 
 def normalize_nyul(image_to_normalize, modality, flair_images):
     nyul_norm = NyulNormalize(output_min_value=0.0, output_max_value=1.0)
     nyul_norm.fit(flair_images, modality=modality)
-    nyul_flair = nyul_norm(image_to_normalize, modality=modality)
+    normalized_image = nyul_norm(image_to_normalize, modality=modality)
 
-    return nyul_flair
+    return normalized_image
 
 
-def normalize_white_stripe(image_to_normalize, modality, norm_value=1.0):
+def normalize_white_stripe(image_to_normalize, modality, norm_value=0.05):
     whitestripe_norm = WhiteStripeNormalize(norm_value=norm_value)
-    white_stripe_flair = whitestripe_norm(image_to_normalize, modality=modality)
+    normalized_image = whitestripe_norm(image_to_normalize, modality=modality)
 
-    return white_stripe_flair
+    return normalized_image
 
 
-def normalize_zscore(image):
-    return (image - np.mean(image)) / np.std(image)
+def normalize_zscore(image_to_normalize, modality):
+    zscore_norm = ZScoreNormalize()
+    normalized_image = zscore_norm(image_to_normalize, modality=modality)
+
+    return normalized_image
 
 
 def generate_scans_paths(data_dir, suffix="flair"):
@@ -70,7 +123,7 @@ def generate_scans_paths(data_dir, suffix="flair"):
     return volumes_path
 
 
-def plot_single_histogram_and_slice(volume, slice_index, brain_mask, filename):
+def plot_single_histogram_and_slice(volume, slice_index, brain_mask, title, filename):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     # Ensure the axes indexing works for multiple or single arrays
@@ -82,7 +135,7 @@ def plot_single_histogram_and_slice(volume, slice_index, brain_mask, filename):
 
     # Plot histogram
     ax_hist.hist(positive_values, bins=100, color='blue', edgecolor='black')
-    ax_hist.set_title(f'Histogram of pixel intensities values.')
+    ax_hist.set_title(f'Histogram of pixel intensities values ({title}).')
     ax_hist.set_xlabel('Pixel intensity')
     ax_hist.set_ylabel('Frequency')
 
@@ -134,20 +187,27 @@ def plot_histograms_one_slice(volumes, slice_index, brain_mask):
     plt.show()
 
 
-def normalize_all_from_dir(data_dir, output_dir, path_from_local_dir: Dict, normalizers, save_histogram_slice_plots=True):
-    modalities_filepaths = fop.get_nii_filepaths(data_dir, path_from_local_dir, shuffle_local_dirs=True, n_patients=2)
+def normalize_all_from_dir(data_dir: str,
+                           output_dir: str,
+                           path_from_local_dir: Dict,
+                           normalizers: List[Normalizer],
+                           save_histogram_slice_plots=True):
+    modalities_filepaths = fop.get_nii_filepaths(data_dir, path_from_local_dir, shuffle_local_dirs=True)
 
     # splitting the datasets into n subsets (n number of normalizers)
     n_normalization = len(normalizers)
     subset_size = len(list(modalities_filepaths.values())[0]) // n_normalization
     normalizers_with_indices_ranges = {normalizer: (i*subset_size, (i+1)*subset_size) for i, normalizer in enumerate(normalizers)}
 
+    if save_histogram_slice_plots:
+        histogram_slice_plot_dir = os.path.join(output_dir, "slices_and_histograms")
+        fop.try_create_dir(histogram_slice_plot_dir)
     # for each modality:
     # load the data and normalize the data with the given normalizer
     # store it in a new directory
     for modality, filepaths in modalities_filepaths.items():
-        modality_path = os.path.join(output_dir, modality)
-        fop.try_create_dir(modality_path)
+        # modality_path = os.path.join(output_dir, modality)
+        # fop.try_create_dir(modality_path)
         for normalizer, indices_range in normalizers_with_indices_ranges.items():
             dedicated_filepaths = filepaths[indices_range[0]: indices_range[1]]
             raw_volumes = []
@@ -156,24 +216,28 @@ def normalize_all_from_dir(data_dir, output_dir, path_from_local_dir: Dict, norm
                volume = nib.load(volume_path).get_fdata()
                raw_volumes.append(volume)
 
-            for i, volume in enumerate(raw_volumes):
-                normalized_volume = normalizer(volume)
-                # normalized_volume = normalizer(volume, raw_volumes)
+            # so far I give as the argument of the function all the raw volumes always, because only one setup exists
+            # TODO: make it more adaptive
+            normalizer.setup([np.array(raw_volumes)])
 
-                # TODO: only substantial part of the brain
-                # TODO: save as 3D volume?
-                patient_file_name = os.path.basename(dedicated_filepaths[i]).split('.')[0]
+            for i, volume in enumerate(raw_volumes):
+                normalized_volume = normalizer(volume, Modality.from_string(modality))
+
+                patient_file_name = dedicated_filepaths[i].split(os.path.sep)[-2]
 
                 # saving histogram and slice plot
                 if save_histogram_slice_plots:
-                    image_path = os.path.join(modality_path, f"{str(normalizer)}_{patient_file_name}.png")
+                    image_path = os.path.join(histogram_slice_plot_dir, f"{str(normalizer)}_{patient_file_name}_{modality}.png")
                     plot_single_histogram_and_slice(normalized_volume,
                                                     slice_index=110,
                                                     brain_mask=volume > 1e-6,
+                                                    title=str(normalizer),
                                                     filename=image_path)
 
                 # saving the volume as a 3D numpy array
-                save_path = os.path.join(modality_path, f"{str(normalizer)}_{patient_file_name}")
+                patient_new_path = os.path.join(output_dir, patient_file_name)
+                fop.try_create_dir(patient_new_path)
+                save_path = os.path.join(patient_new_path, f"{str(normalizer)}_{modality}.npy")
                 np.save(save_path, normalized_volume)
 
 
@@ -203,7 +267,9 @@ def test_every_normalizer():
                           "Nyul": normalize_nyul(image_flair, modality, flair_images),
                           # "White Stripe (normalization scaler=0.2)": normalize_white_stripe(image_flair, norm_value=0.2),
                           # "White Stripe (normalization scaler=0.02)": normalize_white_stripe(image_flair, norm_value=0.02),
-                          "White Stripe (normalization scaler=0.05)": normalize_white_stripe(image_flair, modality, norm_value=0.05)}
+                          "White Stripe (normalization scaler=0.05)": normalize_white_stripe(image_flair, modality, norm_value=0.05),
+                          "z-score": normalize_zscore(image_flair, modality=modality)
+                          }
 
     plot_histograms_one_slice(normalized_volumes, slice_index=110, brain_mask=brain_mask)
 
@@ -218,14 +284,33 @@ def test_every_normalizer():
     # plt.show()
 
 
+def define_normalizers_and_more():
+    normalizers = [Normalizer("nonorm", NoNormalization),
+                   Normalizer("nyul", NyulNormalize, normalizer_kwargs={"output_min_value": -1.0, "output_max_value": 1.0}),
+                   Normalizer("whitestripe", WhiteStripeNormalize, normalizer_kwargs={"norm_value": 0.05}),
+                   Normalizer("ZScore", ZScoreNormalize)
+                   ]
+
+    return normalizers
+
+
 if __name__ == '__main__':
-    test_every_normalizer()
-    # paths_from_local_dirs = {"t1": "*t1.nii.gz", "t2": "*t2.nii.gz", "flair": "*flair.nii.gz"}
-    # normalizers = [Normalizer("WhiteStripe", normalize_white_stripe),
-    #                Normalizer("NoNorm", lambda x: x)]
-    #
-    # normalize_all_from_dir("C:\\Users\\JanFiszer\\data\\mri\\flair_volumes",
-    #                        "C:\\Users\\JanFiszer\\data\\mri\\normalized",
-    #                        paths_from_local_dirs,
-    #                        normalizers)
+    # test_every_normalizer()
+    if configs.config.LOCAL:
+        # data_dir = "C:\\Users\\JanFiszer\\data\\mri\\flair_volumes"
+        data_dir = "C:\\Users\\JanFiszer\\data\\mri\\UCSF-PDGM"
+        output_dir = "C:\\Users\\JanFiszer\\data\\mri\\nomralized-UCSF-PDGM"
+        # output_dir = "C:\\Users\\JanFiszer\\data\\mri\\normalized"
+        paths_from_local_dirs = {"t1": "*t1.nii.gz", "t2": "*t2.nii.gz", "flair": "*flair.nii.gz"}
+    else:
+        data_dir = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/UCSF-PDGM-v3"
+        output_dir = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/UCSF-normalized"
+        paths_from_local_dirs = {"t1": "*T1.nii.gz", "t2": "*T2.nii.gz", "flair": "*FLAIR.nii.gz"}
+
+    normalizers = define_normalizers_and_more()
+
+    normalize_all_from_dir(data_dir,
+                           output_dir,
+                           paths_from_local_dirs,
+                           normalizers)
 
