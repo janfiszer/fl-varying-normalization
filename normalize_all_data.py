158765ef371b094cc8d8ed960eb30668cd39a04c
Jan Fiszer <plgfiszer@login01.ares.cyfronet.pl>
2025-04-03 12:00:30 +0200

    not normalizing - mask scenerio improved

--- normalize_all_data.py
+++ normalize_all_data.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         paths_from_local_dirs = {"t1": "*t1.nii.gz",
                                  "t2": "*t2.nii.gz",
                                  "flair": "*flair.nii.gz",
-                                 # "mask": "*tumor_segmentation.nii.gz"
+                                 "mask": "*tumor_segmentation.nii.gz"
                                  }
     else:
         data_dir = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/UCSF-PDGM-v3"
@@ -21,19 +21,13 @@ if __name__ == '__main__':
         paths_from_local_dirs = {"t1": "*T1.nii.gz",
                                  "t2": "*T2.nii.gz",
                                  "flair": "*FLAIR.nii.gz",
-                                 # "mask": "*tumor_segmentation.nii.gz"
+                                 "mask": "*tumor_segmentation.nii.gz"
                                  }
     normalizers = data_normalization.define_normalizers_and_more()
-
-    data_normalization.demonstrate_normalization(data_dir,
-                                                 output_dir,
-                                                 paths_from_local_dirs,
-                                                 normalizers,
-                                                 n_volumes=10)
-    #
-    # data_normalization.normalize_all_from_dir(data_dir,
-    #                                           output_dir,
-    #                                           paths_from_local_dirs,
-    #                                           normalizers,
-    #                                           mask_file="mask",
-    #                                           )
+    
+    data_normalization.normalize_all_from_dir(data_dir,
+                                              output_dir,
+                                              paths_from_local_dirs,
+                                              normalizers,
+                                              mask_file="mask",
+                                              )
