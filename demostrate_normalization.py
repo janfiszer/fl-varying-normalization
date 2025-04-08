86089cddb6757e655e11e36ed05ce9c64f7f8d9c
Jan Fiszer <plgfiszer@login01.ares.cyfronet.pl>
2025-04-08 15:38:10 +0200

    cleaned

--- demostrate_normalization.py
+++ demostrate_normalization.py
@@ -4,16 +4,12 @@ from src.utils import data_normalization
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    # test_every_normalizer()
     if configs.config.LOCAL:
-        # data_dir = "C:\\Users\\JanFiszer\\data\\mri\\flair_volumes"
         data_dir = "C:\\Users\\JanFiszer\\data\\mri\\UCSF-PDGM"
         output_dir = "C:\\Users\\JanFiszer\\data\\mri\\nomralized-UCSF-PDGM"
-        # output_dir = "C:\\Users\\JanFiszer\\data\\mri\\normalized"
         paths_from_local_dirs = {"t1": "*t1.nii.gz",
                                  "t2": "*t2.nii.gz",
                                  "flair": "*flair.nii.gz",
-                                 # "mask": "*tumor_segmentation.nii.gz"
                                  }
     else:
         data_dir = "/net/pr2/projects/plgrid/plggflmri/Data/Internship/UCSF-PDGM-v3"
@@ -21,7 +17,6 @@ if __name__ == '__main__':
         paths_from_local_dirs = {"t1": "*T1.nii.gz",
                                  "t2": "*T2.nii.gz",
                                  "flair": "*FLAIR.nii.gz",
-                                 # "mask": "*tumor_segmentation.nii.gz"
                                  }
     normalizers = data_normalization.define_normalizers_and_more()
 
