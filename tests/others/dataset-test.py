bc0b16492178ca75d6289ca7bf2be525777ad05f
Jan Fiszer <plgfiszer@login01.ares.cyfronet.pl>
2025-04-09 16:41:30 +0200

    code cleaning

--- tests/others/dataset-test.py
+++ tests/others/dataset-test.py
@@ -1,10 +1,9 @@
 import logging
 import sys
 from os import path
-import torch
 
 from configs import config
-from src.utils.visualization import plot_pred_tigth, plot_all_modalities_and_target
+from src.utils.visualization import plot_all_modalities_and_target
 from src.deep_learning.datasets import SegmentationDataset2DSlices
 
 from torch.utils.data import DataLoader
@@ -17,7 +16,7 @@ if __name__ == '__main__':
 
     dataset = SegmentationDataset2DSlices(data_dir_ares, ["t1", "t2", "flair"], "mask", binarize_mask=True)
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
     first_iteration = True
     for images, targets in dataloader:
@@ -34,7 +33,7 @@ if __name__ == '__main__':
                 else:
                     logging.log(logging.INFO, "The shapes are matching.")
 
-    plot_all_modalities_and_target(images, targets, column_names=["t1", "t2", "flair", "mask"], rotate_deg=270)
+    plot_all_modalities_and_target(images, targets, column_names=["t1", "t2", "flair", "mask"], rotate_deg=270, savepath=path.join(data_dir_ares, "last_batch_dataset_test.jpg"))
 
 
 
