e5c4ebf94f8ea44b79c86816b24c7033c1c1ee5c
JanFiszer <jf.janfiszer@gmail.com>
2025-04-09 13:28:13 +0200

    test working nice

--- tests/others/dataset-test.py
+++ tests/others/dataset-test.py
@@ -1,20 +1,21 @@
+import logging
 import sys
 from os import path
 import torch
 
 from configs import config
-from src.utils.visualization import plot_pred_tigth
+from src.utils.visualization import plot_pred_tigth, plot_all_modalities_and_target
 from src.deep_learning.datasets import SegmentationDataset2DSlices
 
 from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
     if config.LOCAL:
-        data_dir_ares = "C:\\Users\\JanFiszer\\data\\mri\\segmentation_ucsf_whitestripe_test\\train"
+        data_dir_ares = "C:\\Users\\JanFiszer\\data\\mri\\segmentation_ucsf_whitestripe_test\\validation"
     else:
         data_dir_ares = sys.argv[1]
 
-    dataset = SegmentationDataset2DSlices(data_dir_ares, ["t1", "t2", "flair"], "mask")
+    dataset = SegmentationDataset2DSlices(data_dir_ares, ["t1", "t2", "flair"], "mask", binarize_mask=True)
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
 
@@ -27,24 +28,13 @@ if __name__ == '__main__':
                 first_iteration = False
             else:
                 if image.shape != image_shape:
-                    print(f"WARNING: The shapes are different", image_shape, "!=", image.shape)
+                    logging.log(logging.WARNING, f"The shapes are different", image_shape, "!=", image.shape)
                 elif target.shape != target_shape:
-                    print(f"WARNING: The shapes are different", target_shape, "!=", target.shape)
+                    logging.log(logging.WARNING, f"The shapes are different", target_shape, "!=", target.shape)
                 else:
-                    print("good")
+                    logging.log(logging.INFO, "The shapes are matching.")
 
-    target_mask = []
-    t1_images = []
-    t2_images = []
-    flair_images = []
-
-    for image, target in zip(images, targets):
-        t1_images.append(torch.unsqueeze(image[:, 0], 0))
-        t2_images.append(torch.unsqueeze(image[:, 1], 0))
-        flair_images.append(torch.unsqueeze(image[:, 2], 0))
-        target_mask.append(torch.unsqueeze(target, 0))
-
-    plot_pred_tigth([t1_images, t2_images, flair_images, target_mask], savepath="last_batch_dataset.jpg")
+    plot_all_modalities_and_target(images, targets, predictions_list=targets, column_names=["t1", "t2", "flair", "mask"], rotate_deg=270)
 
 
 
