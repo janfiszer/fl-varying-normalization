080ef7b0b7a2f3511140495a3921716e7b6d02c0
JanFiszer <jf.janfiszer@gmail.com>
2025-05-06 10:59:45 +0200

    fl working locally

---
+++ src/fl/clients.py
@@ -0,0 +1,262 @@
+import os
+import os.path
+import pickle
+from pathlib import Path
+import random
+from collections import OrderedDict
+from typing import Dict, Tuple
+
+import flwr as fl
+import numpy as np
+import torch
+from flwr.common.typing import NDArrays, Scalar
+from torch.utils.data import DataLoader
+
+from configs import enums, config as global_config
+from src.deep_learning import models
+from src.deep_learning.datasets import SegmentationDataset2DSlices
+
+
+class ClassicClient(fl.client.NumPyClient):
+    """
+        Overriding all the methods that NumPyClient requires.
+    """
+    def __init__(self, client_id, model: models.UNet, optimizer, data_dir, model_dir):
+        """
+            Constructor
+
+            Parameters
+            ----------
+            client_id:
+                Client representative, string. Usually set to the name of the dataset.
+            model:
+                Client model which he will update during local training
+            optimizer:
+                Model's optimizer, used as the loss function
+            data_dir:
+                Full path to the data directory, which client trains on. Requires to have inside (named exactly):
+                    - train
+                    - test 
+                    - validation
+            model_dir:
+                A directory, inside which a directory based on client_id and client type is created. 
+                There the client saves its model and its potential test plot, 
+        """
+        self.client_id = client_id
+        self.model = model
+        self.train_loader, self.test_loader, self.val_loader = load_data(data_dir,
+                                                                         batch_size=global_config.BATCH_SIZE,
+                                                                         with_num_workers=not global_config.LOCAL)
+
+        self.optimizer = optimizer
+
+        self.history = {f"val_{metric_name}": [] for metric_name in global_config.METRICS}
+
+        self.client_dir = os.path.join(model_dir,
+                                       f"{self.__repr__()}_client_{self.client_id}")
+
+        Path(self.client_dir).mkdir()
+        print(f"Client {client_id} with data from directory: {data_dir}: INITIALIZED\n")
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for val in self.model.state_dict().values()]
+
+    def set_parameters(self, parameters: NDArrays):
+        param_dict = zip(self.model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in param_dict})
+        self.model.load_state_dict(state_dict)
+
+    def fit(self, parameters: NDArrays, config):
+        self.set_parameters(parameters)
+
+        current_round = config["current_round"]
+        print(f"ROUND {current_round}")
+
+        if global_config.LOCAL:
+            plots_dir = None
+        else:
+            plots_dir = f"{self.client_dir}/rd-{current_round}_training_plots"
+
+        history = self.model.perform_train(self.train_loader,
+                                           self.optimizer,
+                                           model_dir=self.client_dir,
+                                           validationloader=self.val_loader,
+                                           epochs=global_config.N_EPOCHS_CLIENT,
+                                        #    plots_dir=plots_dir
+                                           )
+
+        print(f"END OF CLIENT TRAINING\n")
+
+        val_metric_names = [f"val_{metric}" for metric in global_config.METRICS]
+
+        # only validation metrics from the client (ensured by 'val_' suffix)
+        avg_val_metric = {
+            metric_name: sum([metric_value for metric_value in history[metric_name]]) / len(history[metric_name])
+            for metric_name in val_metric_names}
+
+        avg_val_metric["client_id"] = self.client_id  # client_id to keep truck of the loss properly e.g. FedCostW
+
+        return self.get_parameters(config=config), len(self.train_loader.dataset), avg_val_metric
+
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Tuple[float, int, Dict]:
+        self.set_parameters(parameters)
+
+        metrics = self._evaluate(current_round=config["current_round"])
+        metric_without_loss = {k: v for k, v in metrics.items() if k != "val_loss"}
+
+        return metrics["val_loss"], len(self.test_loader.dataset), metric_without_loss
+
+    def _evaluate(self, current_round: int):
+
+        print(f"CLIENT {self.client_id} ROUND {current_round} TESTING...")
+
+        if global_config.LOCAL:
+            plots_path = None
+            plot_filename = None
+        else:
+            plots_path = f"{self.client_dir}/test_plots"
+            plot_filename = f"round-{current_round}"
+
+        metrics = self.model.evaluate(self.test_loader,
+                                    #   plots_path=plots_path,
+                                    #   plot_filename=plot_filename
+                                      )
+
+        print(f"END OF CLIENT TESTING\n\n")
+
+        # adding to the history
+        for metric_name, metric_value in metrics.items():
+            self.history[metric_name].append(metric_value)
+
+        if current_round % global_config.CLIENT_SAVING_FREQ == 0:
+            self.model.save(self.client_dir, f"model-rd{current_round}")
+
+        # saving model and history if it is the last round
+        if current_round == global_config.N_ROUNDS:
+            self.model.save(self.client_dir)
+
+            with open(f"{self.client_dir}/history.pkl", 'wb') as file:
+                pickle.dump(self.history, file)
+
+        return metrics
+
+    def __repr__(self):
+        return "FedAvg"
+
+
+class FedBNClient(ClassicClient):
+    """Changes only the parameters operation (set and get) skipping the normalization layers"""
+
+    # NOT NEEDED (SAME US FEDMRI) 
+    # def get_parameters(self, config) -> NDArrays:
+    #     return [val.cpu().numpy() for name, val in self.model.state_dict().items()]
+    #     # return [val.cpu().numpy() for layer_name, val in self.model.state_dict().items()
+    #             # if "norm" not in layer_name]
+
+    def set_parameters(self, parameters):
+        self.model.train()
+
+        old_state_dict = self.model.state_dict()
+
+        # Excluding parameters of BN layers when using FedBN
+        layer_names = {index: layer_name for index, layer_name in enumerate(old_state_dict.keys())
+                       if "norm" not in layer_name}
+
+        selected_parameters = [parameters[i] for i in layer_names.keys()]
+        param_dict = zip(layer_names.values(), selected_parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in param_dict})
+        self.model.load_state_dict(state_dict, strict=False)
+
+    def __repr__(self):
+        return f"FedBN(batch_norm={global_config.NORMALIZATION})"
+
+
+class FedMRIClient(ClassicClient):
+    "Changes only the parameters operation (set and get) skipping the decoder part. Only encoder in global"
+    def set_parameters(self, parameters: NDArrays):
+        self.model.train()
+
+        old_state_dict = self.model.state_dict()
+
+        layer_names = {index: layer_name for index, layer_name in enumerate(old_state_dict.keys())
+                       if "down" in layer_name or "inc" in layer_name}
+
+        selected_parameters = [parameters[i] for i in layer_names.keys()]
+        param_dict = zip(layer_names.values(), selected_parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in param_dict})
+        self.model.load_state_dict(state_dict, strict=False)
+
+    def __repr__(self):
+        return f"FedMRI()"
+
+
+# def client_from_config(client_id, unet: models.UNet, optimizer, data_dir: str):
+#     """
+#         Returns a client basing on the config variables.
+#         Not used in the starting server and clients from the same file.
+#     """
+#
+#     if global_config.CLIENT_TYPE == enums.ClientTypes.FED_BN:
+#         return FedBNClient(client_id, unet, optimizer, data_dir)
+#
+#     elif global_config.CLIENT_TYPE == enums.ClientTypes.FED_MRI:
+#         return FedMRIClient(client_id, unet, optimizer, data_dir)
+#
+#     else:  # config.CLIENT_TYPE == global_config.ClientTypes.FED_AVG:
+#         return ClassicClient(client_id, unet, optimizer, data_dir)
+
+
+def client_from_string(client_id, unet: models.UNet, optimizer, data_dir: str, client_type_name):
+    """
+        Returns a instance of a class basing on the given string. Requires the model (Pytorch net) and optimizer. 
+        Client ID is a string.
+    """
+    drd = global_config.DATA_ROOT_DIR
+    lr = global_config.LEARNING_RATE
+    rd = global_config.N_ROUNDS
+    ec = global_config.N_EPOCHS_CLIENT
+    d = global_config.now.date()
+    h = global_config.now.hour
+
+    model_dir = f"{drd}/trained_models/model-{client_type_name}-lr{lr}-rd{rd}-ep{ec}-{d}"
+    
+    print(f"Client {client_id} has directory: {model_dir}")
+
+    if client_type_name in ["fedbn"]:
+        return FedBNClient(client_id, unet, optimizer, data_dir, model_dir)
+    elif client_type_name in ["fedmri"]:
+        return FedMRIClient(client_id, unet, optimizer, data_dir, model_dir)
+    elif  client_type_name in ["fedavg"]:
+        return ClassicClient(client_id, unet, optimizer, data_dir, model_dir)
+    
+    else:
+        raise ValueError(f"Given client type ('{client_type_name}') name is invalid.")
+
+
+def load_data(data_dir, batch_size, with_num_workers=True):
+    """
+        Function returning training, test and validation loader based on samely named directories in the given directory (data_dir)
+    """
+    train_dir = os.path.join(data_dir, "train")
+    test_dir = os.path.join(data_dir, "test")
+    val_dir = os.path.join(data_dir, "validation")
+
+    args = (global_config.USED_MODALITIES, global_config.MASK_DIR, True)
+
+    trainset = SegmentationDataset2DSlices([train_dir],  *args)
+    testset = SegmentationDataset2DSlices([test_dir], *args)
+    validationset = SegmentationDataset2DSlices([val_dir], *args)
+
+    if with_num_workers:
+        train_loader = DataLoader(trainset, batch_size=batch_size, num_workers=global_config.NUM_WORKERS,
+                                  pin_memory=True, shuffle=True)
+        test_loader = DataLoader(testset, batch_size=batch_size, num_workers=global_config.NUM_WORKERS,
+                                 pin_memory=True, shuffle=True)
+        val_loader = DataLoader(validationset, batch_size=batch_size, num_workers=global_config.NUM_WORKERS,
+                                pin_memory=True, shuffle=True)
+    else:
+        train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(testset, batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(validationset, batch_size=batch_size, shuffle=True)
+
+    return train_loader, test_loader, val_loader
---
+++ src/fl/strategies.py
@@ -0,0 +1,212 @@
+import time
+import pickle
+import os
+from shutil import copy2
+from pathlib import Path
+import torch
+import logging
+import numpy as np
+
+from functools import reduce
+from torch.utils.data import DataLoader
+
+from src.deep_learning import datasets, models
+from src.utils import files_operations
+from configs import config
+
+import flwr as fl
+from flwr.server.criterion import ClientProxy
+from flwr.common import Scalar, FitRes, Parameters, logger, Metrics, NDArrays, parameters_to_ndarrays, \
+    ndarrays_to_parameters, NDArray
+from flwr.server.strategy import Strategy
+from flwr.server.strategy import FedAdam, FedAvg, FedYogi, FedProx, FedAdagrad, FedAvgM, aggregate
+
+from typing import List, Tuple, Dict, Union, Optional, Type, Callable
+from collections import OrderedDict
+
+
+def create_dynamic_strategy(StrategyClass: Type[Strategy], model: models.UNet, model_dir, *args, **kwargs):
+    """ A function that returns a strategy class instance that will return  
+    """
+    class SavingModelStrategy(StrategyClass):
+        def __init__(self):
+            initial_parameters = [val.cpu().numpy() for val in model.state_dict().values()]
+            super().__init__(initial_parameters=ndarrays_to_parameters(initial_parameters), *args, **kwargs)
+            self.model = model
+            self.model_dir = model_dir
+            self.aggregation_times = []
+            self.best_loss = float('inf')
+
+            Path(self.model_dir).mkdir(exist_ok=config.LOCAL)  # creating directory before to don't get warnings
+            copy2("./configs/config.py", f"{self.model_dir}/config.py")
+
+        def aggregate_fit(
+                self,
+                server_round: int,
+                results: List[Tuple[ClientProxy, FitRes]],
+                failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
+        ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
+            # start counting time
+            start = time.time()
+
+            aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+                        
+            # printing and saving aggregation times
+            aggregation_time = time.time() - start
+            self.aggregation_times.append(aggregation_time)
+            print(f"\n{self.__str__()} aggregation time: {aggregation_time}\n")
+
+            # computing average loss
+            loss_values = [fit_res.metrics["val_loss"] for _, fit_res in results]
+            current_avg_loss = sum(loss_values)/len(loss_values)
+
+            # saving model
+            self.save_model_conditionally(aggregated_parameters, server_round, current_avg_loss=current_avg_loss)
+
+            return aggregated_parameters, aggregated_metrics
+
+        def save_model_conditionally(self, aggregated_parameters, server_round, save_last_round=True, save_intervals=config.SAVING_FREQUENCY, current_avg_loss=None):
+            # saving in intervals
+            if save_intervals:
+                if server_round % config.SAVING_FREQUENCY == 1:
+                    save_aggregated_model(self.model, aggregated_parameters, self.model_dir, server_round)
+
+            # saving the best model
+            if current_avg_loss:
+                if current_avg_loss < self.best_loss:
+                    print(f"Best model with loss {current_avg_loss:.3f}<{self.best_loss:.3f}")
+                    save_aggregated_model(self.model, aggregated_parameters, self.model_dir, server_round, best_model=True)
+                    self.best_loss = current_avg_loss
+                else:
+                    print(f"Best model with loss {current_avg_loss:.3f}>{self.best_loss:.3f}")
+
+            # saving in the last round
+            if save_last_round:
+                if server_round == config.N_ROUNDS:
+                    # model
+                    save_aggregated_model(self.model, aggregated_parameters, self.model_dir, server_round)
+                    # aggregation times
+                    with open(f"{self.model_dir}/aggregation_times.pkl", "wb") as file:
+                        pickle.dump(self.aggregation_times, file)
+
+    return SavingModelStrategy()
+
+
+def save_aggregated_model(model: models.UNet, aggregated_parameters, model_dir, server_round: int, best_model=False):
+    """
+        Takes aggregated parameters and saves them to the model_dir with name describing the current round.
+    """
+    aggregated_ndarrays = fl.common.parameters_to_ndarrays(aggregated_parameters)
+
+    params_dict = zip(model.state_dict().keys(), aggregated_ndarrays)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+
+    # saving the model with an appropriate name
+    model_name = "best.pth" if best_model else f"round{server_round}.pth"
+    torch.save(state_dict, os.path.join(model_dir, model_name))
+
+    # it could have been done using
+    # model.load_state_dict(state_dict)
+    # model.save(model_dir, filename=f"round{server_round}.pth")
+
+    logger.log(logging.INFO, f"Saved round {server_round} aggregated parameters to {model_dir}")
+
+def strategy_from_string(model, strategy_name, evaluate_fn=None):
+    """
+        Returns client object. Basing on the strategy name different aggregation methods are chosen.
+        Asignes appropriate parameters from config if they are needed by the aggreagation method.
+        model_dir is constructed basing on the config_train.
+    """
+
+    # the directory includes the strategy name
+    # so when it is initialized by the string it is created here
+    # by default it takes the name TRAINED_MODEL_SERVER_DIR
+    drd = config.DATA_ROOT_DIR
+    lr = config.LEARNING_RATE
+    rd = config.N_ROUNDS
+    ec = config.N_EPOCHS_CLIENT
+    d = config.now.date()
+    h = config.now.hour
+
+    model_dir = f"{drd}/trained_models/model-{strategy_name}-lr{lr}-rd{rd}-ep{ec}-{d}"
+
+    ## FOR NOW CREATION IN THE STRATEGY CONSTRUCTOR
+    # for optimal from_config usage created in the strategy constructor
+    # files_operations.try_create_dir(model_dir)
+    # copy2("./configs/config_train.py", f"{model_dir}/config.py")
+
+    kwargs = {
+        "min_fit_clients": config.MIN_FIT_CLIENTS,
+        "min_available_clients": config.MIN_AVAILABLE_CLIENTS,
+        "fraction_fit": config.FRACTION_FIT,
+        "on_fit_config_fn": get_on_fit_config(),
+        "evaluate_fn": evaluate_fn,
+        "on_evaluate_config_fn": get_on_eval_config()
+    }
+
+    if strategy_name in ["fedadam", "fedmix", "fedbadam"]:
+        strategy_class = FedAdam
+        kwargs["tau"] = config.TAU
+
+    elif strategy_name in ["fedavg", "fedbn"]:
+        strategy_class = FedAvg
+    else:
+        raise ValueError(f"Wrong starategy name: {strategy_name}")
+
+    return create_dynamic_strategy(strategy_class, model, model_dir, **kwargs)
+
+
+def strategy_from_config(model, evaluate_fn=None):
+    kwargs = {
+        "min_fit_clients": config.MIN_FIT_CLIENTS,
+        "min_available_clients": config.MIN_AVAILABLE_CLIENTS,
+        "fraction_fit": config.FRACTION_FIT,
+        "on_fit_config_fn": get_on_fit_config(),
+        "evaluate_fn": evaluate_fn,
+        "on_evaluate_config_fn": get_on_eval_config()
+    }
+
+    if config.AGGREGATION_METHOD == config.AggregationMethods.FED_ADAM:
+        strategy_class = FedAdam
+        kwargs["tau"] = config.TAU
+    else:  # FedAvg or FedBN
+        strategy_class = FedAvg
+
+    return create_dynamic_strategy(strategy_class, model, **kwargs)
+
+
+# FUNCTIONS
+# used by the strategy to during fit and evaluate
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    # val_metric_names = [f"val_{metric}" for metric in config.METRICS]
+    results = {f"val_metric_name": 0.0 for metric_name in config.METRICS}
+
+    for num_examples, m in metrics:
+        for metric_name in results.keys():
+            results[metric_name] += num_examples * m[metric_name]
+
+    examples = [num_examples for num_examples, _ in metrics]
+
+    for metric_name in results.keys():
+        results[metric_name] /= sum(examples)
+
+    return results
+
+
+def get_on_fit_config():
+    def on_fit_config_fn(server_round: int):
+        fit_config = {"current_round": server_round}
+        if config.CLIENT_TYPE == config.ClientTypes.FED_PROX:
+            fit_config["drop_client"] = False
+
+        return fit_config
+
+    return on_fit_config_fn
+
+
+def get_on_eval_config():
+    def on_eval_config_fn(server_round: int):
+        fit_config = {"current_round": server_round}
+        return fit_config
+
+    return on_eval_config_fn
