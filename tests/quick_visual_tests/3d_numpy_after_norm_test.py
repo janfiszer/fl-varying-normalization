9158d9d134700465bf139ceaa71a5955377b884d
JanFiszer <jf.janfiszer@gmail.com>
2025-05-07 12:16:34 +0200

    plotting more

--- tests/quick_visual_tests/3d_numpy_after_norm_test.py
+++ tests/quick_visual_tests/3d_numpy_after_norm_test.py
@@ -4,12 +4,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\nomralized-UCSF-PDGM\\minmax\\UCSF-PDGM-0429_FU003d_nifti"
+# sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\nomralized-UCSF-PDGM\\minmax\\UCSF-PDGM-0429_FU003d_nifti"
+# sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\from-nonorm-normalized-UCSF-PDGM\\fcm\\UCSF-PDGM-0491_nifti"
+data_path = "C:\\Users\\JanFiszer\\data\\mri\\from-nonorm-normalized-UCSF-PDGM"
+# sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\from-nonorm-normalized-UCSF-PDGM\\nonorm"
+# sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\from-nonorm-normalized-UCSF-PDGM\\minmax"
+# sample_patient_path = "C:\\Users\\JanFiszer\\data\\mri\\from-nonorm-normalized-UCSF-PDGM\\minmax"
+
 slice_index = 111
-for volume_filename in os.listdir(sample_patient_path):
-    volume_path = os.path.join(sample_patient_path, volume_filename)
-    volume = np.load(volume_path)
-    plt.imshow(volume[:, :, slice_index], cmap="grey")
-    plt.title(volume_filename)
-    plt.colorbar()
-    plt.show()
+
+for ds_name in ["fcm", "minmax", "nonorm", "nyul", "whitestripe", "zscore"]:
+    sample_patient_path = os.path.join(data_path, ds_name, "UCSF-PDGM-0491_nifti")
+    for volume_filename in os.listdir(sample_patient_path):
+        volume_path = os.path.join(sample_patient_path, volume_filename)
+        volume = np.load(volume_path)
+        plt.imshow(volume[:, :, slice_index], cmap="gray")
+        plt.title(f"{ds_name} {volume_filename}")
+        plt.colorbar()
+        plt.show()
